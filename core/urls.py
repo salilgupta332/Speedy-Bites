1c657289625a997d29bf47a07f64710dfdb06215
salilgupta332 <salilgupta332@gmail.com>
2025-07-06 02:49:04 +0530

    Update routing path

--- core/urls.py
+++ core/urls.py
@@ -5,8 +5,8 @@ from . import views
 from .views import home, add_menu_item, edit_menu_item, delete_menu_item,landing_page
 
 urlpatterns = [
-    path('', landing_page, name='landing'),
-    path('', views.home, name='home'),
+    path('', landing_page, name='landing'),              # This will load by default on "/"
+    path('menu/', home, name='home'),                    # Admin panel
     path('add/', add_menu_item, name='add_menu_item'),
     path('edit/<str:item_id>/', edit_menu_item, name='edit_menu_item'),
     path('delete/<str:item_id>/', delete_menu_item, name='delete_menu_item'),
