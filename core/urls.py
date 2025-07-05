fe808e4fb3438612ffa3ee1045f3d1ceaad5e0a7
salilgupta332 <salilgupta332@gmail.com>
2025-07-06 03:25:20 +0530

    Update route

--- core/urls.py
+++ core/urls.py
@@ -1,17 +1,13 @@
 # core/urls.py
 
-from django.urls import path , include
-from django.contrib import admin
-from . import views
-from .views import home, add_menu_item, edit_menu_item, delete_menu_item,landing_page
+from django.urls import path
+from .views import home, landing_page, add_menu_item, edit_menu_item, delete_menu_item
 
 urlpatterns = [
     
-    path('', include('core.urls')),
-    path('', landing_page, name='landing'),              # This will load by default on "/"
-    path('menu/', home, name='home'),                    # Admin panel
+    path('', landing_page, name='landing'),                     # Landing page
+    path('menu/', home, name='home'),                           # Admin dashboard
     path('add/', add_menu_item, name='add_menu_item'),
     path('edit/<str:item_id>/', edit_menu_item, name='edit_menu_item'),
     path('delete/<str:item_id>/', delete_menu_item, name='delete_menu_item'),
-    path('admin/django/', admin.site.urls),
 ]
