8f6b7d8c111b65302aedc96b7bfec220485674d3
salilgupta332 <salilgupta332@gmail.com>
2025-07-06 07:54:37 +0530

    Updated Landing Page Added

--- core/urls.py
+++ core/urls.py
@@ -15,5 +15,5 @@ urlpatterns = [
     path('register/', views.register, name='register'),
     path('login/', auth_views.LoginView.as_view(template_name='core/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
-    path('admin/dashboard/', views.admin_landing, name='admin_landing'),
+    path('admin-panel/', views.admin_landing, name='admin_landing'),
 ]
