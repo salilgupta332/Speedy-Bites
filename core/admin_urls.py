d04357d8edb60ab678bfdc4b37c7088ea543420a
salilgupta332 <salilgupta332@gmail.com>
2025-07-10 21:14:03 +0530

    Login Functionality added

--- core/admin_urls.py
+++ core/admin_urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path('admin-register/', views.admin_register, name='admin_register'),
-    
+    path('admin-login/', views.admin_login, name='admin_login'),
     path('admin-dashboard/', views.admin_dashboard, name='admin_dashboard'),
     
 ]
