42b9637886c9f83e0c7b1e9b6511910df0006d3f
salilgupta332 <salilgupta332@gmail.com>
2025-07-10 20:46:04 +0530

    Admin Route Updated

--- core/admin_urls.py
+++ core/admin_urls.py
@@ -4,7 +4,7 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin-register/', views.admin_register, name='admin_register'),
-    path('admin-login/', views.admin_login, name='admin_login'),
+    
     path('admin-dashboard/', views.admin_dashboard, name='admin_dashboard'),
-    path('admin-logout/', views.admin_logout, name='admin_logout'),
+    
 ]
