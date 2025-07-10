4cb6f1e67d6977e3f7fb7de78c56adc364b829b3
salilgupta332 <salilgupta332@gmail.com>
2025-07-10 20:55:55 +0530

    Admin Route Updated

--- core/admin_urls.py
+++ core/admin_urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
-from django.contrib.auth import views as auth_views
+
 
 urlpatterns = [
     path('admin-register/', views.admin_register, name='admin_register'),
--- speedy_bites/urls.py
+++ speedy_bites/urls.py
@@ -16,10 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include 
-from core.views import home, add_menu_item 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    # path('admin/', admin.site.urls),
     path('', include('core.urls')),
-    path('add/', add_menu_item, name='add_menu_item'),
+    
+     path('admin/', include('core.admin_urls')),  
 ]
