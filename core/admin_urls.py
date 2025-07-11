160e2c1270bc5ed7ddc25fbbbd8855868d2be5e1
salilgupta332 <salilgupta332@gmail.com>
2025-07-11 10:01:33 +0530

    Log out functionality added

--- core/admin_urls.py
+++ core/admin_urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('admin-login/', views.admin_login, name='admin_login'),
     path('admin-dashboard/', views.admin_dashboard, name='admin_dashboard'),
     path('admin-dashboard/Menu-dashboard/', views.menu_dashboard, name='menu_dashboard'),
+    path('admin-logout/', views.admin_logout, name='admin_logout'),
     
 ]
