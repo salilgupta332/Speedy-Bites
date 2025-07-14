dc5bfee3eab885a4fe4e3f1e14192be0b4fbb8cd
salilgupta332 <salilgupta332@gmail.com>
2025-07-15 00:44:53 +0530

    Routes Update

--- core/urls.py
+++ core/urls.py
@@ -1,23 +1,31 @@
-# core/urls.py
-
 from django.urls import path , include
-from .views import  landing_page, add_menu_item, edit_menu_item, delete_menu_item , admin_register
+from .views import (
+    landing_page, 
+    add_menu_item, 
+    edit_menu_item, 
+    delete_menu_item, 
+    admin_register,
+    user_register,
+    user_login,
+)
 from . import views
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    
-    path('', landing_page, name='landing'),                     # Landing page
-                             # Admin dashboard
+    path('', landing_page, name='landing'),
+
+    # Menu routes (admin)
     path('add/', add_menu_item, name='add_menu_item'),
     path('edit/<str:item_id>/', edit_menu_item, name='edit_menu_item'),
     path('delete/<str:item_id>/', delete_menu_item, name='delete_menu_item'),
-    
-    
+
+    # Auth
     path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
     path('admin-panel/', views.admin_dashboard, name='admin_dashboard'),
     path('admin-register/', admin_register, name='admin_register'),
+
+    # Admin and User routes
     path('admin/', include('core.admin_urls')),
-    path('user/register/', views.user_register, name='user_register'),
-    path('user/login/', views.user_login, name='user_login'),
+    path('user/register/', user_register, name='user_register'),
+    path('user/login/', user_login, name='user_login'),
 ]
