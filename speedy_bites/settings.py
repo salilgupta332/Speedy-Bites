c26788ccbb30adc898152e452de39ac64430f471
salilgupta332 <salilgupta332@gmail.com>
2025-07-24 04:12:45 +0530

    CSS Updated

--- speedy_bites/settings.py
+++ speedy_bites/settings.py
@@ -31,7 +31,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-5#-%d3@uu#%b872+gse-$d!$9e-^%b_7m7ym0m+i2_*y#7s=j3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "*").split(",")
 
@@ -39,9 +39,9 @@ STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
-    BASE_DIR / "core/static",
 ]
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') 
 
 # Application definition
 
