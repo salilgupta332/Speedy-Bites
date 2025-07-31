a750f3891179ae9c106be8659954856f8bccd87f
salilgupta332 <salilgupta332@gmail.com>
2025-08-01 05:07:14 +0530

    Bugfix-3

--- speedy_bites/settings.py
+++ speedy_bites/settings.py
@@ -92,12 +92,12 @@ WSGI_APPLICATION = 'speedy_bites.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Password validation
